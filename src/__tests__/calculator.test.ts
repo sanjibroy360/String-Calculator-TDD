9c05b49f7e04a6619f0271cfeb7bc16731288883
sanjibroy360 <sanjibroy0098@gmail.com>
2025-03-29 02:15:49 +0530

    test: Add missing tests for the Calculator class to validate negative number handling and maxAllowedNumber filtering

--- src/__tests__/calculator.test.ts
+++ src/__tests__/calculator.test.ts
@@ -22,4 +22,35 @@ describe("Calculator", () => {
     const calculator: Calculator = new Calculator(numbers);
     expect(calculator.add()).toBe(expectedSum);
   });
+
+  test("should throw an exception for a single negative number when allowOnlyPositiveNumbers is true", () => {
+    const calculator: Calculator = new Calculator([1, 2, 3, -4]);
+    calculator["allowOnlyPositiveNumbers"] = true;
+    expect(() => calculator.add()).toThrow("negative numbers not allowed -4");
+  });
+
+  test("should throw an exception for multiple negative numbers when allowOnlyPositiveNumbers is true", () => {
+    const calculator: Calculator = new Calculator([1, -2, 3, -4, 5, -6]);
+    calculator["allowOnlyPositiveNumbers"] = true;
+    expect(() => calculator.add()).toThrow(
+      "negative numbers not allowed -2,-4,-6"
+    );
+  });
+
+  test("should not throw any exception for negative numbers when allowOnlyPositiveNumbers is false", () => {
+    const calculator: Calculator = new Calculator([1, -2, 3, -4, 5, -6]);
+    calculator["allowOnlyPositiveNumbers"] = false;
+    expect(calculator.add()).toBe(-3)
+  });
+
+  test("should ignore numbers bigger than maxAllowedNumber", () => {
+    const calculator: Calculator = new Calculator([1, 1001, 2]);
+    calculator["maxAllowedNumber"] = 1000;
+    expect(calculator.add()).toBe(3);
+  });
+
+  test("should not ignore any number when maxAllowedNumber is not set", () => {
+    const calculator: Calculator = new Calculator([1, 1001, 2]);
+    expect(calculator.add()).toBe(1004);
+  });
 });
